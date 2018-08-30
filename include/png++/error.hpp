d410ce71bc7a8f22a226c925fe7cf218fa6cfaaa
David Banks <dave@hoglet.com>
2018-08-30 10:01:41 +0100

    Fix compilation issue on MAC OS X

--- include/png++/error.hpp
+++ include/png++/error.hpp
@@ -100,7 +100,7 @@ namespace png
             strerror_s(buf, ERRBUF_SIZE, errnum);
             return std::string(buf);
 #else
-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE
+#if defined(__APPLE__) || ((_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE)
             strerror_r(errnum, buf, ERRBUF_SIZE);
             return std::string(buf);
 #else
